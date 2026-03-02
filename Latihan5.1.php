75d5d4e0a2115a60bf5e22c7ecd6e959e4caf746
PBO-FaridNur-10112031 <faridnurfahrudin174@gmail.com>
2026-03-02 11:16:05 +0700

    Add files via upload

---
+++ Latihan5.1.php
@@ -0,0 +1,12 @@
+<?php
+$nilai = 75;
+
+if ($nilai >= 60) {
+    echo "Lulus";
+} elseif ($nilai < 60) {
+    echo "Tidak Lulus";
+} else {
+    echo "Maaf nilai yang diinputkan tidak sesuai";
+}
+
+?>
